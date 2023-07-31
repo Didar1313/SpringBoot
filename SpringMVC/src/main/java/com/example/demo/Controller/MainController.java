565b4a4490baaf9c8c50b5bdd95e3735700ac441
IB Didar <73778140+Didar1313@users.noreply.github.com>
2023-07-31 14:20:31 +0600

    Model&ModelMap(MVC)

--- SpringMVC/src/main/java/com/example/demo/Controller/MainController.java
+++ SpringMVC/src/main/java/com/example/demo/Controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -9,5 +11,15 @@ public class MainController {
 	@RequestMapping("/home")
 	public String homePage() {
 		return "HomePage.html";
+	}
+	@RequestMapping("/addProgrammer")
+	public String addProgrammer(@RequestParam int pId, @RequestParam String pName,
+			@RequestParam String pLang,Model model) {
+		model.addAttribute("pId", pId);
+		model.addAttribute("pName", pName);
+		model.addAttribute("pLang", pLang);
+		
+	
+		return "ProgrammerInfo.html";
 	}
 }
